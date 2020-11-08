3980cecf369596a2bf23d52c21c9606a52d0511b
Sachith Dassanayake <sachith.net@gmail.com>
2020-11-08 12:23:58 +0000

    Return SVG

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function draw(sel, chord, opts) {
   return new ChordBox(sel, opts).draw(chord);
 }
 
+function getSvg(sel, chord, opts) {
+  return new ChordBox(null, opts).drawAndReturnSvg(chord);
+}
+
 export {
-  POSITIONS, SHAPES, ChordBox, build, draw,
+  POSITIONS, SHAPES, ChordBox, build, draw, getSvg,
 };
